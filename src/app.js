451475a74c41d21ac37a24dba353a88abb2f61b7
NagShankar <nag.shankar6@gmail.com>
2020-03-25 16:13:48 +0530

    boilerplate added

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+//importing CSS
+import './styles/style.scss';
+import 'normalize.css/normalize.css';
+
+ReactDOM.render(
+<p>This is boilerplate</p>,
+document.getElementById('app')
+)
+
+
